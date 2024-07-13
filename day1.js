c2a7448b17e69280410701e655870ad1c1ef12ee
Sandip Neupane <sandipneupane775@gmail.com>
2024-07-13 22:12:53 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,49 @@
+// Variables and Data Types
+
+// Activity 1
+//Declare a variable using "var", assign it a number and log the value to the console
+var  num = 49;
+console.log(num);
+
+//Declare a variable using "let", assign it a string and log the value to the console
+let str = "Sandip"
+console.log(str);
+
+// Activity 2
+//Declare a variable using "const", assign it a boolean value and log the value to the console
+const  boy = true;
+console.log(boy);
+
+// Activity 3
+// create a variables of different data types(number, string ,boolean object, array ) and log each variable's type using "typeof" operator
+const num1 = 50;
+console.log(typeof num1);
+
+const str1 = "Sandip"
+console.log(typeof str1);
+
+const boy1 = true;
+console.log(typeof boy1);
+
+const obj = {
+    namee: "Sandip",
+    age: 21
+}
+console.log(typeof obj);
+
+const arr = [1, 2, 3, 4, 5];
+console.log(typeof arr);
+
+// Activity 4
+//Declare a varible using "let", assign it a initial value and reassign a new value and log both values
+let car = "Porsche"
+console.log("The initial value is", car);
+car = "Toyota"
+console.log("The final value is ", car);
+
+// Activity 5
+//Declare a varible using "const", assign it a initial value and reassign a new value and log both values and analyse the output
+const book = "Harry Potter"
+console.log(book);
+book = "Chemistry"
+console.log(book);
